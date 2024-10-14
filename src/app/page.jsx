8419af4febfa3fedb1953d558cafef5ee8c6a407
Nicolas <njcaceres04@gmail.com>
2024-10-14 10:57:50 -0500

    undo commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -26,15 +26,13 @@ export default function Home() {
 		setShowModal(false);
 	};
 	return (
-		<div className='flex flex-col flex-1'>
+		<main className='w-full flex flex-col '>
 			{showModal && <ModalLogin onClose={handleClose} />}
-			<div className='relative flex-1'>
-				<Image
-					src='/placeholder.svg'
+			<section className='w-full shadow-xl'>
+				<img
+					src='https://www.unisabana.edu.co/fileadmin/_processed_/8/8/csm_Ad-portas-edificio-unisabana_5925d366bd.jpg'
 					alt='Modern university building'
-					width={400}
-					height={200}
-					className='w-full h-48 object-cover'
+					className='w-full'
 				/>
 				<button className='absolute left-2 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white rounded-full p-1'>
 					<ChevronLeft size={24} />
@@ -42,30 +40,30 @@ export default function Home() {
 				<button className='absolute right-2 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white rounded-full p-1'>
 					<ChevronRight size={24} />
 				</button>
-			</div>
+			</section>
 
-			<div className='p-6 flex-1 flex gap-2 '>
-				<div className='flex-1 p-5'>
+			<section className='p-5 justify-between flex gap-5'>
+				<div className='flex flex-col w-full'>
 					<button
-						className='w-full bg-green-600 text-white py-2 rounded-md font-semibold mb-4'
+						className='bg-[#028747] text-white p-2 rounded-md font-semibold'
 						onClick={() => {
 							setShowModal(true);
 						}}
 					>
 						Iniciar sesión
 					</button>
-					<p className='text-sm flex justify-center gap-2'>
-						No tienes cuenta?
-						<Link href='/register' className='text-blue-500'>
+					<span className='text-xs flex justify-between items-center min-w-full'>
+						<p>No tienes cuenta?</p>
+						<Link href='/register' className='text-blue-500 w-full'>
 							Crea una
 						</Link>
-					</p>
+					</span>
 				</div>
-				<p className='flex-1'>
+				<p className='flex text-sm'>
 					Conecta con tus compañeros y vive viajes inolvidables. Tu comunidad
 					universitaria viaja contigo...
 				</p>
-			</div>
-		</div>
+			</section>
+		</main>
 	);
 }
--- src/app/ui/layouts/Footer.jsx
+++ src/app/ui/layouts/Footer.jsx
@@ -1,11 +1,11 @@
 import { Facebook, Instagram, Youtube } from 'lucide-react';
 export default function Footer() {
 	return (
-		<div className='bg-green-600 text-white p-4 flex justify-between items-center'>
-			<div className='flex space-x-4'>
+		<div className='bg-[#028747] text-white p-4 flex justify-between items-center'>
+			<div className='flex gap-1 justify-center items-center'>
 				<Facebook size={24} />
 				<Instagram size={24} />
-				<Youtube size={24} />
+				<Youtube size={30} />
 			</div>
 			<div className='text-lg font-bold'>WHEEL US</div>
 		</div>
