b6b46ac26831b48a7b5466fa7737e3219ae8bad5
Nicolas <njcaceres04@gmail.com>
2024-10-16 16:32:24 -0500

    added social media links

--- src/app/ui/layouts/Footer.jsx
+++ src/app/ui/layouts/Footer.jsx
@@ -1,7 +1,7 @@
 import { Facebook, Instagram, Youtube } from 'lucide-react';
 import Image from 'next/image';
 import logo from '/public/images/logo.png';
-import { usePathname } from 'next/navigation'; // Import usePathname
+import { usePathname } from 'next/navigation';
 
 export default function Footer() {
 	const pathname = usePathname();
@@ -12,9 +12,27 @@ export default function Footer() {
 	return (
 		<div className='bg-[#028747] text-white p-4 flex justify-between items-center'>
 			<div className='flex items-center justify-between gap-2'>
-				<Facebook size={24} />
-				<Instagram size={24} />
-				<Youtube size={30} />
+				<a
+					href='https://www.facebook.com/groups/950625678281628/?locale=es_LA'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<Facebook size={24} />
+				</a>
+				<a
+					href='https://www.instagram.com/parkingwheelsunisabana/?hl=es'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<Instagram size={24} />
+				</a>
+				<a
+					href='https://www.youtube.com/user/sabanahoy'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<Youtube size={30} />
+				</a>
 			</div>
 			<div className='text-lg flex gap-2 font-semibold'>
 				<Image
