4d869df3dc265df049b71546bf1fd85a019b0d6c
Nicolas <njcaceres04@gmail.com>
2024-10-18 21:49:50 -0500

    added routeCard

--- src/app/(pages)/driverDashboard/page.jsx
+++ src/app/(pages)/driverDashboard/page.jsx
@@ -1,16 +1,16 @@
 'use client';
-import AvailableTripCard from '@/app/ui/components/userDashboard/AvailableTripCard';
+import RouteCard from '@/app/ui/components/driverDashboard/routeCard';
 export default function DashboardPage() {
 	const tripCards = [];
 
-	for (let i = 0; i < 1; i++) {
-		tripCards.push(<AvailableTripCard key={i} />);
+	for (let i = 0; i < 3; i++) {
+		tripCards.push(<RouteCard key={i} />);
 	}
 
 	return (
 		<section className='flex flex-col justify-center w-full items-center bg-white'>
-			<section className='w-full h-screen p-5 pt-0 flex justify-center'>
-				<div className='bg-[#D9D9D9] shadow-gray-600 shadow-md bg-opacity-50 w-[98%] h-[70%] overflow-auto p-3 border-solid border-[1px] border-[#696C70] border-opacity-50 rounded-lg grid grid-cols-1 lg:grid-cols-2 gap-2'>
+			<section className='w-full h-screen p-5 flex justify-center'>
+				<div className='bg-[#D9D9D9] shadow-gray-600 shadow-md bg-opacity-50 w-[98%] h-fit gap-3 overflow-auto p-3 border-solid border-[1px] border-[#696C70] border-opacity-50 rounded-lg grid grid-cols-1 lg:grid-cols-2 justify-center items-center'>
 					{tripCards}
 				</div>
 			</section>
--- src/app/ui/modals/Loader.jsx
+++ src/app/ui/modals/Loader.jsx
@@ -3,7 +3,7 @@ export default function Loader() {
 		<div className='fixed inset-0 flex items-center justify-center z-50'>
 			<div className='bg-white rounded-lg shadow-lg p-5 w-[300px] h-[350px]'>
 				<div className='flex flex-col justify-center items-center gap-10 text-[#028747] font-bold text-lg'>
-					Válidando Información ...
+					Validando Información ...
 					<div className='w-[150px] h-[150px] border-[10px] border-t-[10px] border-t-[#028747] border-gray-200 rounded-full animate-spin'></div>
 				</div>
 			</div>
