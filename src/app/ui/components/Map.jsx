1824792039b481bd1ec636335b5dc1ff9b4a7e39
Nicolas <njcaceres04@gmail.com>
2024-11-15 16:36:36 -0500

    Added local host api key acces

--- src/app/ui/components/Map.jsx
+++ src/app/ui/components/Map.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
 const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+if (!googleMapsApiKey) {
+	console.error(
+		'Google Maps API key is not defined. Please check your .env file.'
+	);
+}
 
 const containerStyle = {
 	width: '100%',
@@ -8,17 +15,19 @@ const containerStyle = {
 };
 
 const center = {
-	lat: 4.8609881, // Latitud de Nueva York
-	lng: -74.0332402879779, // Longitud de Nueva York
+	lat: 4.8609881, // Latitude for Universidad de La Sabana (example)
+	lng: -74.0332402879779, // Longitude for Universidad de La Sabana
 };
 
 const MyMapComponent = () => {
-	return (
+	return googleMapsApiKey ? (
 		<LoadScript googleMapsApiKey={googleMapsApiKey}>
 			<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
 				<Marker position={center} />
 			</GoogleMap>
 		</LoadScript>
+	) : (
+		<p>Error: Google Maps API key is missing.</p>
 	);
 };
 
